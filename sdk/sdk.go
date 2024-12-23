7786fad4621e00e503e1dc2bc7d15e6b946515a0
ChenYunDa <chenyunda218@gmail.com>
2024-12-23 18:52:45 +0800

    test

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -1,13 +1,34 @@
 package sdk
 
-import "github.com/alterminal/auth/model"
+import (
+	"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/alterminal/auth/api"
+	"github.com/alterminal/auth/model"
+)
 
 type Client struct {
 	BaseUrl     string
 	AccessToken string
-	Namespace   *string
 }
 
-func (c *Client) CreateAccount(account *model.Account) (*model.Account, error) {
-	return nil, nil
+func (c *Client) CreateAccount(request api.CreateAccountRequest) (account model.Account, err error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	body, _ := json.Marshal(request)
+	req, _ := http.NewRequest("POST", c.BaseUrl+"/account", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Access-Token", c.AccessToken)
+	resp, _ := client.Do(req)
+	respBody, _ := io.ReadAll(resp.Body)
+	json.Unmarshal(respBody, &account)
+	fmt.Println(resp)
+	return account, nil
 }
